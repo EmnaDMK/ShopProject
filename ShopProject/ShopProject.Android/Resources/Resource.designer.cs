934ececfec3cda58776c07581fcd614e5d5c02ba
Emna Dammak <e.dammak@root.sa>
2018-03-16 12:18:51 +0100

    Final Version

--- ShopProject/ShopProject.Android/Resources/Resource.designer.cs
+++ ShopProject/ShopProject.Android/Resources/Resource.designer.cs
@@ -2195,26 +2195,26 @@ namespace ShopProject.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f020119
-			public const int avd_hide_password_1 = 2130837785;
-			
 			// aapt resource value: 0x7f02011a
-			public const int avd_hide_password_2 = 2130837786;
+			public const int avd_hide_password_1 = 2130837786;
 			
 			// aapt resource value: 0x7f02011b
-			public const int avd_hide_password_3 = 2130837787;
+			public const int avd_hide_password_2 = 2130837787;
+			
+			// aapt resource value: 0x7f02011c
+			public const int avd_hide_password_3 = 2130837788;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f02011c
-			public const int avd_show_password_1 = 2130837788;
-			
 			// aapt resource value: 0x7f02011d
-			public const int avd_show_password_2 = 2130837789;
+			public const int avd_show_password_1 = 2130837789;
 			
 			// aapt resource value: 0x7f02011e
-			public const int avd_show_password_3 = 2130837790;
+			public const int avd_show_password_2 = 2130837790;
+			
+			// aapt resource value: 0x7f02011f
+			public const int avd_show_password_3 = 2130837791;
 			
 			// aapt resource value: 0x7f020056
 			public const int Cart = 2130837590;
@@ -2768,11 +2768,11 @@ namespace ShopProject.Droid
 			// aapt resource value: 0x7f02010d
 			public const int notification_icon_background = 2130837773;
 			
-			// aapt resource value: 0x7f020117
-			public const int notification_template_icon_bg = 2130837783;
-			
 			// aapt resource value: 0x7f020118
-			public const int notification_template_icon_low_bg = 2130837784;
+			public const int notification_template_icon_bg = 2130837784;
+			
+			// aapt resource value: 0x7f020119
+			public const int notification_template_icon_low_bg = 2130837785;
 			
 			// aapt resource value: 0x7f02010e
 			public const int notification_tile_bg = 2130837774;
@@ -2800,6 +2800,9 @@ namespace ShopProject.Droid
 			
 			// aapt resource value: 0x7f020116
 			public const int Search = 2130837782;
+			
+			// aapt resource value: 0x7f020117
+			public const int Settings = 2130837783;
 			
 			static Drawable()
 			{
